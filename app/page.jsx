c2e7d4dfd5cbe8db6fb4fa25c2811f2c15dfcb52
Abderrezak 2715101 <151585908+Malek3031@users.noreply.github.com>
2024-06-04 18:17:33 -0400

    Branche abderrezak (#13)
    * Setpage-Logo
    
    * Footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
 'use client';
 import { usePathname } from 'next/navigation';
 import Accueil from '../Composents/Accueil';
+import About from '../Composents/About';
 import Reservation from '../Composents/Reservation';
-import Contact from '../Composents/Contact';  
-import Connexion from '../Composents/Connexion';  
+import Contact from '../Composents/Contact';
+import Connexion from '../Composents/Connexion';
 import styles from './page.module.css';
 
 export default function Page() {
@@ -11,7 +12,8 @@ export default function Page() {
 
   return (
     <div className={styles.page}>
-      {pathname === '/' && <Accueil />}
+      {pathname === '/accueil' && <Accueil />}
+      {pathname === '/about' && <About />}
       {pathname === '/reservation' && <Reservation />}
       {pathname === '/contact' && <Contact />}
       {pathname === '/connexion' && <Connexion />}
