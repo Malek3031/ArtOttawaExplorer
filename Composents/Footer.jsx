946d0ff282b618b52ac72fbc9667aea0ceb2abb6
Abderrezak 2715101 <151585908+Malek3031@users.noreply.github.com>
2024-06-04 18:12:43 -0400

    Update Footer.jsx

--- Composents/Footer.jsx
+++ Composents/Footer.jsx
@@ -1,9 +1,1 @@
-import styles from './Footer.module.css';
 
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <div>&copy; ArtOttawa 2024</div>
-    </footer>
-  );
-}
