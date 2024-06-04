5231b2154c69d2e8b2bceeaffe3bee8d1a13c3c2
Malek3031 <2715101@collegelacite.ca>
2024-06-04 16:19:54 -0400

    Footer

--- Composents/Footer.jsx
+++ Composents/Footer.jsx
@@ -1,9 +1,20 @@
 import styles from './Footer.module.css';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div>&copy; ArtOttawa 2024</div>
+      <div className={styles.content}>
+        <div className={styles.copy}>
+          &copy; ArtOttawa 2024
+        </div>
+        
+        <div className={styles.socials}>
+          <a href="#" className={styles.social}><FaFacebook /></a>
+          <a href="#" className={styles.social}><FaTwitter /></a>
+          <a href="#" className={styles.social}><FaInstagram /></a>
+        </div>
+      </div>
     </footer>
   );
 }
