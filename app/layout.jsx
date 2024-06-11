d1e7023092d99bda0a9c6716619208bcb03e8fd2
2705436 <152231218+2705436@users.noreply.github.com>
2024-06-11 00:27:05 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,21 @@
 'use client';
+import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import Head from 'next/head';
 import Header from '../Composents/Header';
 import Footer from '../Composents/Footer';
+import Accueil from '../Composents/Accueil';
+import About from '../Composents/About';
+import Reservation from '../Composents/Reservation';
+import Contact from '../Composents/Contact';
+import Connexion from '../Composents/Connexion';
 import styles from './layout.module.css';
 import './globals.css';
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  const [page, setPage] = useState('accueil');
+
   return (
     <html lang="fr">
       <Head>
@@ -13,9 +23,14 @@ export default function RootLayout({ children }) {
         <meta name="description" content="Découvrez l'art et la culture d'Ottawa, la capitale du Canada. Explorez notre collection d'art unique et réservez des visites guidées pour une expérience inoubliable." />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <body className={styles.body}>
-        <Header />
-        <main className={styles.main}>
+      <body>
+        <Header setPage={setPage} />
+        <main className={pathname === '/connexion' ? styles.connexionBackground : ''}>
+          {page === 'accueil' && <Accueil />}
+          {page === 'about' && <About />}
+          {page === 'reservation' && <Reservation />}
+          {page === 'contact' && <Contact />}
+          {page === 'connexion' && <Connexion />}
           {children}
         </main>
         <Footer />
